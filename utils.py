2b94f8138a0d284e96ab525d8ee5ef64dc165291
CK <179700749@qq.com>
2026-04-26 23:21:00 +0800

    neural network lab

--- utils.py
+++ utils.py
@@ -5,3 +5,13 @@ def load_data(file_name):
     X = data[:,:-1]
     y = data[:,-1]
     return X, y
+
+def load_data_npy():
+    X = np.load("data/X.npy")
+    y = np.load("data/y.npy")
+    X = X[0:1000]
+    y = y[0:1000]
+    return X, y
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
